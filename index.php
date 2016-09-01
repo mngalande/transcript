9f5fd99904efd63663041c437953d9644a32cbb0
mngalande <ngalandemartin@gmail.com>
2016-09-01 16:57:40 +0000

    started the log in page

--- index.php
+++ index.php
@@ -1,5 +1,58 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Transcript| Log in</title>
+
+    <!-- Bootstrap -->
+    <link href="public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="public/bootstrap/js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+    <div align="center">
+      <img src="public/images/Unima_Logo.jpg" height="180px" width="150px"></img>
+      <p><font size="5">Chancellor College</font></p>
+      <p><font size="4">Transcript Data Entry System</font></p>
+      <br>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4 col-sm-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title panel-title-primary">Log in</h3>
+                </div>
+
+                <div class="panel-body">
+                  <form action="" method="post">
+                    <div class="input-group">
+                      <label for="username">Username</label>
+                      <input type="text" id="username" name="username" class="form-control" placeholder="Username">
+                    </div>
+                    <br>
+                    <div class="input-group">
+                      <label for="password">Password</label>
+                      <input type="password" id="passsword" name="password" class="form-control" placeholder="Password">
+                    </div>
+                    <br>
+                    <div class="input-group">
+                      <input class="button button-md button-primary" name="submit" type="submit" value="Submit">
+                    </div>
+
+                  </form>
+                </div>
+            </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
 
 
-
-?>
