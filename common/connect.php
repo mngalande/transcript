95808985b510017d1ca976eded1711d8f394a75d
jshaibu <jrshaibu@gmail.com>
2016-09-06 18:06:17 +0200

    Changes made to connect.php

--- common/connect.php
+++ common/connect.php
@@ -15,6 +15,7 @@ class conn
 			conn::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			conn::$con->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
 			conn::$con->setAttribute(PDO::ATTR_PERSISTENT, true);
+			conn::$con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 		}
 		
 		return conn::$con;
@@ -24,13 +25,41 @@ class conn
 		die($e->getMessage() . '<br />' . $e->getTraceAsString());
 	}
 	
-	public static function query($var)
+	public static function update($var, $params){
+		$sql = conn::db()->prepare($var);
+		$sql->execute($params);
+		
+		return true;
+	}
+	
+	public static function query($var, $params)
 	{
-		$sql = conn::db()->prepare($var['query']);
-		$sql->execute($var['args']);
+		$sql = conn::db()->prepare($var);
+		$sql->execute($params);
 		
 		return $sql;
 	}
 }
+
+/*
+
+Examples
+
+// remember to include on your script: require_once "../common/connect.php";
+
+$sql = conn::update("INSERT INTO tblprogrammes SET 
+					 ProgrammeID = ProgrammeID = ?", 
+					 array($_POST['ProgrammeID']));
+					 
+if($sql)
+	"Successfully updated";
+
+$sql = conn::query("SELECT * FROM tblprogrammes 
+					WHERE ProgrammeID = :ProgrammeID", 
+					array(':ProgrammeID' => 1));
+
+while($row = $sql->fetch())
+	echo $row['programmename'];
+*/
 
 ?>
