fc7807aa8960de639246001f3efb4451600f019b
Roland Misomali <rmisomali@cc.ac.mw>
2016-09-09 08:36:01 +0200

    student form validation

--- clerk/index.php
+++ clerk/index.php
@@ -237,8 +237,7 @@ $(function()
   var programmes = [<?php 
     for ( $i = 0; $i < count($programmes_array); $i++) { 
     echo "{value: '$programmes_array[$i]', data: '$programmes_array[$i]'},"; 
-  } ?>
-  ];
+  } ?>];
         
     $('#programme').autocomplete(
     {
